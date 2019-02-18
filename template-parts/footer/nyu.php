f0965906a378ced7c43909bebaa94cda8273e557
Rachel Cherry <rachel@bamadesigner@com>
2019-02-18 13:33:39 -0800

    Adding NYU footer base

---
+++ template-parts/footer/nyu.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template part for displaying the NYU footer.
+ *
+ * @package wp_rig
+ */
+
+namespace WP_Rig\WP_Rig;
+
+?>
+<footer id="colophon" class="site-footer">
+	<div class="site-info">
+		<ul class="site-footer-menu">
+			<li>New York University</li>
+			<li><a href="#">Accessibility</a></li>
+		</ul>
+	</div>
+</footer>
