c5de7e7ae040b0afb2d46188fd4a4bd24928a3d2
Rachel Cherry <bamadesigner@gmail.com>
2019-03-11 05:13:52 -0400

    Implementing can_display_hero() and display_hero()

--- template-parts/header/hero.php
+++ template-parts/header/hero.php
@@ -8,11 +8,11 @@
  *      off on a post by post basis?
  * - Do we want to use a separate image for the header?
  */
-if ( ! is_singular() || ! has_post_thumbnail() ) {
+
+namespace WP_Rig\WP_Rig;
+
+if ( ! wp_rig()->can_display_hero() ) {
     return;
 }
 
-?>
-<aside class="site-hero">
-    <?php the_post_thumbnail( array( 2000, 675 ) ); ?>
-</aside>
+wp_rig()->display_hero();
