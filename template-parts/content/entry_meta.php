d48c6f3e3a6a04f05cd8094d9028dfb823aa4925
Rachel Cherry <bamadesigner@gmail.com>
2019-04-10 23:33:31 -0400

    Make the post author a link to the author archive

--- template-parts/content/entry_meta.php
+++ template-parts/content/entry_meta.php
@@ -33,7 +33,8 @@ $author_string = '';
 if ( post_type_supports( $post_type_obj->name, 'author' ) ) {
 
 	$author_string = sprintf(
-		'<span class="author vcard">%s</span>',
+		'<span class="author vcard"><a class="url fn n" href="%1$s">%2$s</a></span>',
+		esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
 		esc_html( get_the_author() )
 	);
 }
