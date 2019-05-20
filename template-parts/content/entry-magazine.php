02ce4292df22db8eb65e0794082c9357dfd67a35
Rachel Cherry <bamadesigner@gmail.com>
2019-05-19 22:55:11 -0400

    Adding unique post IDs for magazine posts

--- template-parts/content/entry-magazine.php
+++ template-parts/content/entry-magazine.php
@@ -8,7 +8,7 @@
 namespace WP_Rig\WP_Rig;
 
 ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class( 'entry' ); ?>>
+<article id="post-<?php the_ID(); ?>-magazine" <?php post_class( 'entry' ); ?>>
 	<a class="post-entry--link" href="<?php the_permalink(); ?>" aria-label="<?php echo esc_attr( get_the_title() ); ?>">
 		<?php
 
