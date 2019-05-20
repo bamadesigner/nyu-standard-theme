16c3915c529be92b1fa332a087ed50990537c00d
Rachel Cherry <bamadesigner@gmail.com>
2019-05-19 22:46:48 -0400

    Removing aria-hidden from entry post thumbnail

--- template-parts/content/entry_thumbnail.php
+++ template-parts/content/entry_thumbnail.php
@@ -29,7 +29,7 @@ if ( is_singular( get_post_type() ) ) {
 	<?php
 } else {
 	?>
-	<a class="post-thumbnail" href="<?php the_permalink(); ?>" aria-hidden="true">
+	<a class="post-thumbnail" href="<?php the_permalink(); ?>">
 		<?php
 
 		the_post_thumbnail(
