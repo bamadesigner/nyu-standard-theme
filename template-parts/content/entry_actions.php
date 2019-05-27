f86a724beb0c93d5c8c6ae551264c907fc5e849f
Rachel Cherry <bamadesigner@gmail.com>
2019-05-27 11:57:17 -0500

    Don’t show entry-actions if no displayed actions

--- template-parts/content/entry_actions.php
+++ template-parts/content/entry_actions.php
@@ -7,10 +7,19 @@
 
 namespace WP_Rig\WP_Rig;
 
+$show_comments = ! is_singular( get_post_type() ) && ! post_password_required() && post_type_supports( get_post_type(), 'comments' ) && comments_open();
+
+$edit_post_link = get_edit_post_link();
+
+if ( ! $show_comments && ! $edit_post_link ) {
+	return;
+}
+
 ?>
 <div class="entry-actions">
 	<?php
-	if ( ! is_singular( get_post_type() ) && ! post_password_required() && post_type_supports( get_post_type(), 'comments' ) && comments_open() ) {
+
+	if ( $show_comments ) {
 		?>
 		<span class="comments-link">
 			<?php
@@ -33,21 +42,22 @@ namespace WP_Rig\WP_Rig;
 		<?php
 	}
 
-	edit_post_link(
-		sprintf(
-			wp_kses(
-				/* translators: %s: post title */
-				__( 'Edit <span class="screen-reader-text">%s</span>', 'wp-rig' ),
-				array(
-					'span' => array(
-						'class' => array(),
-					),
-				)
-			),
-			get_the_title()
-		),
-		'<span class="edit-link">',
-		' </span>'
-	);
+	if ( $edit_post_link ) :
+		?>
+		<span class="edit-link">
+			<a class="post-edit-link" href="<?php echo esc_url( $edit_post_link ); ?>">
+				<?php
+
+				printf(
+					__( 'Edit <span class="screen-reader-text">%s</span>', 'wp-rig' ),
+					get_the_title()
+				);
+
+				?>
+			</a>
+		</span>
+		<?php
+	endif;
+
 	?>
 </div><!-- .entry-actions -->
